a1711ad6808fbcfecc8f7b3ba05b8155c57aa112
Amdzak <ahmadzulfikar133@gmail.com>
2023-09-03 11:51:11 +0700

    add files

--- 03. Casting Tipe Data/Main.py
+++ 03. Casting Tipe Data/Main.py
@@ -4,3 +4,13 @@ Casting
 casting adalah merubah tipedata menjadi tipedata lain
 
  """
+
+nilai_data = 10
+
+dataFloat  = float(nilai_data)
+dataString = str(nilai_data)
+dataBool   = bool(nilai_data)
+
+print("nilai = ",dataFloat," jenis :",type(dataFloat))
+print("nilai = ",dataString," jenis :",type(dataString))
+print("nilai = ",dataBool," jenis :",type(dataBool))
