91fd26f4f7e482e0a20f73eea16a64ba261b8704
Amdzak <ahmadzulfikar133@gmail.com>
2023-09-03 22:12:14 +0700

    add files

---
+++ 08. Operator Assigment/Main.py
@@ -0,0 +1,39 @@
+""" 
+Operator Assigment
+
+operator assigment merupakan sebuah operator penyingkat dari sebuah operasi aritmatika
+beberapa operator yang bisa di assigmentkan adalah 
+    +=      :   memiliki arti a = a + b
+    -=      :   Memiliki arti a = a - b
+    /=      :   Memiliki arti a = a / b
+    *=      :   Memiliki arti a = a * b
+    %=      :   Memiliki arti a = a % b
+    //=     :   Memiliki arti a = a // b
+    **=     :   Memiliki arti a = a ** b
+"""
+
+dataA = 2000
+dataB = 3
+
+dataA += dataB
+print("hasil nilai dari += ",dataA)
+
+dataA -= dataB
+print("hasil nilai dari -= ",dataA)
+
+dataA **= dataB
+print("hasil nilai dari **= ",dataA)
+
+dataA //= dataB
+print("hasil nilai dari //= ",dataA)
+
+dataA /= dataB
+print("hasil nilai dari /= ",dataA)
+
+dataA %= dataB
+print("hasil nilai dari %= ",dataA)
+
+dataA *= dataB
+print("hasil nilai dari *= ",dataA)
+
+
